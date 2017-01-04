2ba1716b2685b63565d0b1b48abf42a1ccf5b411
César Gutiérrez <cesarypunto@gmail.com>
2017-01-04 14:55:02 +0100

    added kitten

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/webhook', function (req, res) {
 // handler receiving messages
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging
-    for (i = 0; i < events.length; i++) {
+    for (i = 0; i < events.length-1; i++) {
         var event = events[i]
         if (event.message && event.message.text) {
             if (!kittenMessage(event.sender.id, event.message.text)) {
